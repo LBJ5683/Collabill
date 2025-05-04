5646fa57e5dc8a6b385d3efdcb05c9fd62a9197a
LBJ5683 <bjie5683@gmail.comm>
2025-05-04 21:39:36 +0800

    add AdSense to dashboard

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,7 +1,18 @@
 import { metadata } from './metadata';
+import Script from 'next/script'; // 加載 Google AdSense 所需的元件
 
 export { metadata };
 
 export default function DashboardLayout({ children }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7785145306323259"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+      {children}
+    </>
+  );
 }
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,17 @@ import { metadata } from './metadata';
 
 export { metadata };
 
-export default function DashboardLayout({ children }) {
-  return <>{children}</>;
+export default function RootLayout({ children }) {
+  return (
+    <html lang="zh-Hant">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7785145306323259"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
